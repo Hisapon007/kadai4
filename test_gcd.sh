c56aef504ae01c0779583727cce74d3f10f3c71e
Hisapon007 <kimitaka.hisano007@gmail.com>
2022-08-07 01:18:32 +0900

    rechange run.yaml

--- test_gcd.sh
+++ test_gcd.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
 
-sh gcd.sh 84 16
+sh /home/z22084kh/kadai4/gcd.sh 84 16
 
 if [ $? -ne 0 ]; then
     echo "error"
 fi
 
-sh gcd.sh 84 16 10
+sh /home/z22084kh/kadai4/gcd.sh 84 16 10
 
 if [ $? -ne 0 ]; then
     echo "sh gcd.sh 84 16 10 is error"
 fi
 
-sh gcd.sh 84
+sh /home/z22084kh/kadai4/gcd.sh 84
 
 if [ $? -ne 0 ]; then
     echo  "sh gcd.sh 84 is error"
 fi
 
-sh gcd.sh
+sh /home/z22084kh/kadai4/gcd.sh
 
 if [ $? -ne 0 ]; then
     echo "sh gcd.sh is error"
 fi
 
-sh gcd.sh -84 -16
+sh /home/z22084kh/kadai4/gcd.sh -84 -16
 
 if [ $? -ne 0 ]; then
     echo "sh gcd.sh -84 -16 is error"
 fi
 
-sh gcd.sh aaa bbb
+sh /home/z22084kh/kadai4/gcd.sh aaa bbb
 
 if [ $? -ne 0 ]; then
     echo "sh gcd.sh aaa bbb is error"
