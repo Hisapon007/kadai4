52ad2e900a1312ae27151b7eb288a1ceeb16c7c0
Hisapon007 <kimitaka.hisano007@gmail.com>
2022-08-07 09:05:54 +0900

    rechange files

--- test_gcd.sh
+++ test_gcd.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
 
-sh /home/z22084kh/kadai4/gcd.sh 84 16
+sh gcd.sh 84 16
 
 if [ $? -ne 0 ]; then
     echo "error"
 fi
 
-sh /home/z22084kh/kadai4/gcd.sh 84 16 10
+sh gcd.sh 84 16 10
 
 if [ $? -ne 0 ]; then
     echo "sh gcd.sh 84 16 10 is error"
 fi
 
-sh /home/z22084kh/kadai4/gcd.sh 84
+sh gcd.sh 84
 
 if [ $? -ne 0 ]; then
     echo  "sh gcd.sh 84 is error"
 fi
 
-sh /home/z22084kh/kadai4/gcd.sh
+sh gcd.sh
 
 if [ $? -ne 0 ]; then
     echo "sh gcd.sh is error"
 fi
 
-sh /home/z22084kh/kadai4/gcd.sh -84 -16
+sh gcd.sh -84 -16
 
 if [ $? -ne 0 ]; then
     echo "sh gcd.sh -84 -16 is error"
 fi
 
-sh /home/z22084kh/kadai4/gcd.sh aaa bbb
+sh gcd.sh aaa bbb
 
 if [ $? -ne 0 ]; then
     echo "sh gcd.sh aaa bbb is error"
